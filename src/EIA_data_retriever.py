2c11b681c97ea8f31fba5ee2be85cdd2a6612046
Nima Doroud <ndoroud@gmail.com>
2020-01-29 14:44:14 -0500

    retriever

--- src/EIA_data_retriever.py
+++ src/EIA_data_retriever.py
@@ -1,41 +1,75 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 22 09:49:18 2020
-
+January 2020
 @author: nima
 """
+###############################################################################
 #
 #
-import requests
+import boto3
+import io
 import json
+import pandas
+import psycopg2
+import requests
 #
 #
-sid = "EBA.CAL-ALL.D.H" #input()
-eia_key_file = "/home/nima/Data/project/keys/eia.key"
-data_filename = "/home/nima/Data/project/EIA/"+sid+".json"
+s3 = boto3.resource('s3')
 #
 #
-# Input API link and API key.
+with open("project_directory.txt","r") as pdir:
+    project_folder = pdir.read().strip()
+#
+#
+with open(project_folder+"/keys/psql.json","r") as file:
+    psql_settings = json.load(file)
+#
+#
+settings = ""
+for key in psql_settings.keys():
+    settings = settings +" "+ key + "=" + psql_settings[key]
+#
+#
+eba_regions = {}
+with open(project_folder+"/lists/eba_regions.csv","r") as ebaregions:
+    for line in ebaregions:
+        reg = line.strip().split(",")
+        eba_regions[reg[0]] = reg[1:]
+#
 #
 eia_api = "http://api.eia.gov/series/"
-with open(eia_key_file,"r") as key:
+with open(project_folder+"/keys/eia.key","r") as key:
     eia_key=key.read()
 #
 #
-# Set the parameters for the API call.
 #
-parameters = {"api_key": eia_key,
-              "series_id": str(sid),
-              "out": "json"}
+#
+###############################################################################
+#
+#
+siddict = {"demand":"D","net-generation-solar":"NG.SUN","net-generation":"NG"}
+#
+#
+def parameters(region,series):
+    return {"api_key": eia_key,"series_id": "EBA."+region+"-ALL."+siddict[series]+".H","out": "json"}
+#
 #
 #
 # Make API call.
 #
-response = requests.get(eia_api,parameters)
-data = response.json()
+for ser in siddict.keys():
+    for region in eba_regions.keys():
+        response = requests.get(eia_api,parameters(region,ser))
+        data = pandas.DataFrame(response.json()['series'][0]['data']).set_axis(['timestamp', 'demand'], axis='columns', inplace=False)
 #
 #
-file = open(data_filename,"w")
-with open(data_filename, 'w') as outfile:
-    json.dump(data, outfile)
+        data['timestamp'] = pandas.to_datetime(data['timestamp'],infer_datetime_format=True)
+        data = data.set_index('timestamp')
+#
+#
+        csv_buffer = io.StringIO()
+        data.to_csv(csv_buffer)
+        s3.Object("nima-s3", "eia/EBA."+region+"-ALL."+siddict[ser]+".H.csv").put(Body=csv_buffer.getvalue())
+#
+#
